c0cea41e27cb1f604aa70eaf86bea9e7f7b90d53
Satvik Batra <satvikbatra@gmail.com>
2022-06-07 19:19:52 +0530

    user & admin crud api's initialised

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router } from "express";
 import requestRouter from "./request";
 import userRouter from "./user";
+import adminRouter from "./admin";
+
 const router = Router();
 
 router.use("/request", requestRouter);
 router.use("/user", userRouter);
+router.use("/admin", adminRouter);
 router.get("/", (req: Request, res: Response) => {
   res.send("Hey there, this is the api route");
 });
